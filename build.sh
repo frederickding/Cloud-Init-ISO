58803e4489a42497dc811d04f2540c8f0a14d4de
Frederick J. Ding <frederick@dingcorp.com>
2014-12-12 01:49:12 -0500

    Add build script

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Building image..."
+
+genisoimage -output init.iso -volid cidata -joliet -rock user-data meta-data
+
+FILESIZE=$(stat -c %s init.iso 2>/dev/null)
+COLUMNS=$(tput cols)
+if [[ $FILESIZE > 0 ]]; then
+	printf 'init.iso (%d bytes) ... done!\n' $FILESIZE
+else
+	printf 'Something went wrong while trying to make init.iso\n'
+fi
+
