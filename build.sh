d1dbe919a7b7f94b213d3ad148d5f710d317a7d5
Frederick J. Ding <frederick@dingcorp.com>
2014-12-13 22:49:57 -0500

    Allow specifying output filename

--- build.sh
+++ build.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
+FILENAME='init.iso'
 
-echo "Building image..."
+if [ $# -eq 1 ]; then
+	FILENAME=$1
+	echo "Building image to $FILENAME ..."
+elif [ $# -gt 1 ]; then
+	echo 'Usage: ./build.sh [filename]'
+	exit 1
+else
+	echo "Building image to init.iso ..."
+fi
 
-genisoimage -output init.iso -volid cidata -joliet -rock user-data meta-data 2>build.log
+genisoimage -output $FILENAME -volid cidata -joliet -rock user-data meta-data 2>build.log
 
-FILESIZE=$(stat -c %s init.iso 2>/dev/null)
+FILESIZE=$(stat -c %s $FILENAME 2>/dev/null)
 COLUMNS=$(tput cols)
 if [[ $FILESIZE > 0 ]]; then
-	printf 'init.iso (%d bytes) ... done!\n' $FILESIZE
+	printf '%s (%d bytes) ... done!\n' $FILENAME $FILESIZE
 else
-	printf 'Something went wrong while trying to make init.iso\n'
+	printf 'Something went wrong while trying to make %s\n' $FILENAME
 fi
 
