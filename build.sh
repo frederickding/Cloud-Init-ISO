5cdc4c6d212d2edf6e80a96761c1fe8600761e52
Frederick Ding <frederick@frederickding.com>
2017-08-26 17:10:54 -0400

    Update CI variable names to GitLab 9+ format
    - Reverts bba962477de41bc322f4589ff81588fb0ed9897e

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ FILENAME='init.iso'
 # if we are in a git repository, name the ISO after the branch, date, and commit hash
 if [ -v CI ]; then
 	echo "We are in a CI/build environment."
-	FILENAME="$CI_BUILD_REF_SLUG-init-$(date -u '+%Y%m%d').$CI_BUILD_REF.iso"
+	FILENAME="$CI_COMMIT_REF_SLUG-init-$(date -u '+%Y%m%d').$CI_COMMIT_SHA.iso"
 else
 	IS_GIT=$(git rev-parse --is-inside-work-tree 2> /dev/null)
 	if [ $IS_GIT ]; then
